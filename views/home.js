b8980380ea2d1a8d7d46d9857ff69a5e08822d5b
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-08-02 16:11:23 +1000

    Updated Home Page

--- views/home.js
+++ views/home.js
@@ -2,6 +2,10 @@ export function HomeView(BASE_PATH) {
     return `
         <header>
             <img src="${BASE_PATH}/assets/home/cropped_hero_upscaled.avif" alt="Large truck moving rubble from a construction site">
+            <div class="hero-overlay">
+                <h1>Welcome to <span>ONTIME</span></h1>
+                <p>Your trusted partner in <b>waste management</b> solutions</p>
+            </div>
         </header>
     `;
 }
