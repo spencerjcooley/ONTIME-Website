e33f0374926757549f06098b526f24eab1644992
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-08-03 15:55:03 +1000

    Updated Contact Page

--- views/contact.js
+++ views/contact.js
@@ -1,9 +1,36 @@
 export function ContactView() {
     return `
-    <section>
-        <h1>Contact Us</h1>
-        <p>Email: info@otgroup.com.au</p>
-        <p>Phone: 1300 866 937</p>
-    </section>
+        <header>
+            <img src="../assets/contact/banner.avif" alt="Large truck moving rubble from a construction site">
+            <div class="hero-overlay">
+                <h1>Contact <span>US</span></h1>
+            </div>
+        </header>
+
+        <section class="split-section">
+            <div class="split-text">
+                <h2>Contact Us</h2>
+                <ul>
+                    <li><b>Telephone</b><a href="tel:1300866937"><br>1300 866 937</a></li><br>
+                    <li><b>Email</b><a href="mailto:info@otgroup.com.au"><br>info@otgroup.com.au</a></li><br>
+                    <li><b>Head Office</b><br>Floor 0, 27 Stanley Rd, NSW 2565</li>
+                </ul>
+            </div>
+
+            <div class="contact-form">
+                <form action="#" method="post">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" required />
+
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <label for="message">Message</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
+
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
+        </section>
     `;
 }
